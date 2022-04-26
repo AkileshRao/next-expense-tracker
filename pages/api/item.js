49f06c57a277954a0b06dcfcb970ae9d676ae525
Akilesh Rao <akilesh.rao@routemobile.com>
2022-04-26 13:58:25 +0530

    Month wise filter logic added

--- pages/api/item.js
+++ pages/api/item.js
@@ -26,4 +26,54 @@ export default async function handler(req, res) {
             })
         }
     }
+
+    if (method == "GET") {
+        const itemList = await prisma.item.findMany({
+            orderBy: [{
+                date: 'desc'
+            }],
+            select: {
+                date: true
+            },
+            distinct: ['date'],
+            where: {
+                date: {
+                    gt: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+                    lt: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0)
+                }
+            }
+        })
+
+        let items = await Promise.all(itemList.map(item => prisma.item.findMany({
+            select: {
+                description: true,
+                amount: true,
+                id: true,
+                date: true,
+                mode: true
+            },
+            where: {
+                date: item.date
+            }
+        })))
+
+
+        let expensesSum = await prisma.item.groupBy({
+            by: ['mode'],
+            _sum: {
+                amount: true
+            }
+        })
+
+        return {
+            props: {
+                items: JSON.parse(JSON.stringify(items)),
+                total: {
+                    income: expensesSum[0]._sum.amount,
+                    expense: expensesSum[1]._sum.amount
+                }
+            }
+        }
+
+    }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
-import { Flex, IconButton, Box, Text, Heading, Divider, Input, Button, Menu, MenuButton, MenuList, MenuItem } from '@chakra-ui/react';
+import { Flex, IconButton, Box, Text, Heading, Divider, Input, Button, Menu, MenuButton, Select, MenuList, MenuItem, MenuItemOption, MenuOptionGroup } from '@chakra-ui/react';
 import Head from 'next/head'
 import { useRouter } from 'next/router';
 import { AiOutlinePlus } from 'react-icons/ai'
-import { useEffect, useContext } from 'react';
+import { useEffect, useContext, useState } from 'react';
 import { BsChevronDown } from 'react-icons/bs'
 import prisma from '../lib/prisma'
 import { AppContext } from '../state/context';
@@ -11,6 +11,7 @@ export default function Dashboard({ items, total }) {
   const router = useRouter()
   const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   const state = useContext(AppContext)
+  const [currentMonth, setCurrentMonth] = useState(months[new Date().getMonth()]);
   useEffect(() => {
 
   }, [])
@@ -21,6 +22,10 @@ export default function Dashboard({ items, total }) {
     router.push('/item')
   }
 
+  const changeMonth = (month) => {
+    setCurrentMonth(month)
+  }
+
   return (
     <div >
       <Head>
@@ -29,15 +34,11 @@ export default function Dashboard({ items, total }) {
 
       <Flex direction={'column'} gap={3}>
         <Flex justifyContent={'center'}>
-          <Menu>
-            <MenuButton shadow={'md'} style={{ width: "fit-content", minWidth: "100px" }} colorScheme={'teal'} rightIcon={<BsChevronDown />} as={Button}>{months[new Date().getMonth()]}</MenuButton>
-            <MenuList value={months[new Date().getMonth()]}>
-              {months.map(month => (
-                <MenuItem key={month}>{month}</MenuItem>
-              ))}
-              <MenuItem></MenuItem>
-            </MenuList>
-          </Menu>
+          <Select value={currentMonth} onChange={event => changeMonth(event.target.value)} bgColor={"white"} shadow={"sm"}>
+            {
+              months.map(month => <option value={month} key={month}>{month}</option>)
+            }
+          </Select>
         </Flex>
         <Flex align={'center'} gap={10} p='5' bgColor={"white"} shadow={'md'}>
           <Flex direction={'column'} align={'center'}>
@@ -56,25 +57,30 @@ export default function Dashboard({ items, total }) {
           </Flex>
         </Flex>
 
-        <Flex direction={'column'} gap={'3'}>
-          {items.map((item, index) => (
-            <Box key={index} shadow={'md'} bgColor={'white'}>
-              <Flex ps={2} pe={2} pt={2} pb={1}>
-                <Text fontSize={'xs'} color={'blackAlpha.600'}>{item[0].date.slice(0, 10)}</Text>
-              </Flex>
+        {
+          items.length > 0 ? (<Flex direction={'column'} gap={'3'}>
+            {items.map((item, index) => (
+              <Box key={index} shadow={'md'} bgColor={'white'}>
+                <Flex ps={2} pe={2} pt={2} pb={1}>
+                  <Text fontSize={'xs'} color={'blackAlpha.600'}>{item[0].date.slice(0, 10)}</Text>
+                </Flex>
 
-              <Divider />
-              {item.map(itemEntry => (
-                <Box key={itemEntry.id} bgColor={'white'} >
-                  <Flex px={3} py={2} onClick={() => editItem(itemEntry)} alignItems={'center'} justifyContent={'space-between'} className='transaction-item'>
-                    <Text>{itemEntry.description}</Text>
-                    <Text>{itemEntry.mode == "Expense" ? "-" : "+"}{itemEntry.amount}</Text>
-                  </Flex>
-                </Box>
-              ))}
-            </Box>
-          ))}
-        </Flex>
+                <Divider />
+                {item.map(itemEntry => (
+                  <Box key={itemEntry.id} bgColor={'white'} >
+                    <Flex px={3} py={2} onClick={() => editItem(itemEntry)} alignItems={'center'} justifyContent={'space-between'} className='transaction-item'>
+                      <Text>{itemEntry.description}</Text>
+                      <Text>{itemEntry.mode == "Expense" ? "-" : "+"}{itemEntry.amount}</Text>
+                    </Flex>
+                  </Box>
+                ))}
+              </Box>
+            ))}
+          </Flex>) : (
+            <Heading as={'h1'} size={'2xl'}>No items found!</Heading>
+          )
+        }
+
 
       </Flex>
 
@@ -83,45 +89,3 @@ export default function Dashboard({ items, total }) {
   )
 }
 
-export async function getServerSideProps() {
-  const itemList = await prisma.item.findMany({
-    select: {
-      date: true
-    },
-    distinct: ['date']
-  })
-
-  let items = await Promise.all(itemList.map(item => prisma.item.findMany({
-    select: {
-      description: true,
-      amount: true,
-      id: true,
-      date: true,
-      mode: true
-    },
-    where: {
-      date: item.date
-    }
-  })))
-
-
-  let expensesSum = await prisma.item.groupBy({
-    by: ['mode'],
-    _sum: {
-      amount: true
-    }
-  })
-
-
-
-  return {
-    props: {
-      items: JSON.parse(JSON.stringify(items)),
-      total: {
-        income: expensesSum[0]._sum.amount,
-        expense: expensesSum[1]._sum.amount
-      }
-    }
-  }
-
-};
